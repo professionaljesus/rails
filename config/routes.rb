6fd2b7b88a0ee78abe2efdfc4d99ef9b040bd43a
professionaljesus <gustaf-96@hotmail.com>
2019-03-26 00:31:52 +0100

    boi

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 	root 'static_pages#home'
-	  resources :users
+	resources :users
+
+	get  '/signup',  to: 'users#new'
+	post '/signup',  to: 'users#create'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
